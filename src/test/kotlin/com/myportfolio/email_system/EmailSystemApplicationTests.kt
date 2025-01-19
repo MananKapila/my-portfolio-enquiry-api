cf500c4b3a51c741592d729523ae8247e0950160
Manan <manankapiladps@gmail.com>
2025-01-19 18:56:20 +0530

    removing testcase

--- src/test/kotlin/com/myportfolio/email_system/EmailSystemApplicationTests.kt
+++ src/test/kotlin/com/myportfolio/email_system/EmailSystemApplicationTests.kt
@@ -1,13 +1,13 @@
-package com.myportfolio.email_system
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class EmailSystemApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+//package com.myportfolio.email_system
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class EmailSystemApplicationTests {
+//
+//	@Test
+//	fun contextLoads() {
+//	}
+//
+//}
